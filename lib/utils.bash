1ca29ac4752e0ce3a769a36bec1b9e172991b93d
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-08-11 21:18:36 +0700

    fix install_path in install_version()

--- lib/utils.bash
+++ lib/utils.bash
@@ -53,7 +53,7 @@ download_release() {
 install_version() {
   local install_type="$1"
   local version="$2"
-  local install_path="$3"
+  local install_path="$3/bin"
 
   if [ "$install_type" != "version" ]; then
     fail "asdf-$TOOL_NAME supports release installs only"
@@ -61,7 +61,7 @@ install_version() {
 
   (
     mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    cp -r "$ASDF_DOWNLOAD_PATH"/$tool_cmd "$install_path"
 
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
@@ -70,7 +70,6 @@ install_version() {
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful in $install_path!"
-    ls $install_path
   ) || (
     rm -rf "$install_path"
     fail "An error ocurred while installing $TOOL_NAME $version."
