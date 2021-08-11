87371402039ba0d88e39a852a7de15af9732a298
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-08-11 21:01:32 +0700

    debug install_version()

--- lib/utils.bash
+++ lib/utils.bash
@@ -63,7 +63,9 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert trivy executable exists.
+    ls $ASDF_DOWNLOAD_PATH
+    ls $install_path
+
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
